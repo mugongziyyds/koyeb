35cd601547eb6a0a180d5510703724ae107b4cf4
mumu <96757543+mugongziyyds@users.noreply.github.com>
2022-05-13 12:41:56 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "b5250e2c-704e-4087-bc12-2c95adc17e4d",
                         "alterId": 0
                     }
                 ],
